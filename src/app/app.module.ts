f63f2083710bd8f7e892635633b893fa674c3a4b
thinh <capicorn22121999@gmail.com>
2020-11-09 22:05:20 +0700

    article app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
+import { HomePageComponent } from './home-page/home-page.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CanActivate, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { RightContentComponent } from './right-content/right-content.component';
+import { LeftContentComponent } from './left-content/left-content.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RightContentComponent,
+    LeftContentComponent,
+    ArticleDetailComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    // BrowserModule
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'article-detail', component: ArticleDetailComponent },
+      { path: '', component: HomePageComponent, pathMatch: 'full' },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
